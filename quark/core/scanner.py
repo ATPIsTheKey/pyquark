eae9268a5fe491c273b29eaffddbf5ce6df143be
roland <roland.fmn@gmail.com>
2020-11-28 20:32:50 +0200

    Added scanning of strings. Cleaned up some code.

--- quark/core/scanner.py
+++ quark/core/scanner.py
@@ -2,8 +2,7 @@ from quark.core.token_ import (
     Token, TokenTypes, single_char_tokens, double_char_tokens, triple_char_tokens, keyword_tokens
 )
 
-from typing import List, Tuple, Union
-
+from typing import List, Tuple, Union, Generator
 
 __all__ = [
     'QuarkScanner', 'QuarkScannerError'
@@ -16,7 +15,7 @@ class QuarkScannerError(Exception):
 
 
 class QuarkScanner:
-    def __init__(self, source: str, ignore_skippables=True):
+    def __init__(self, source: str, ignore_skippables: bool = True):
         self._source = source
 
         self._ignore_skippables = ignore_skippables
@@ -25,7 +24,7 @@ class QuarkScanner:
         self._source_start, self._source_pos = 0, 0
         self._column_pos, self._line_pos = 0, 0
 
-    def reset(self, source: str, ignore_skippables=True):
+    def reset(self, source: str, ignore_skippables: bool = True):
         self.__init__(source, ignore_skippables)
 
     @property
@@ -34,21 +33,37 @@ class QuarkScanner:
 
     @property
     def _current_char(self) -> str:
-        return self._source[self._source_pos] if not self._reached_end_of_source() else None
+        return (
+            self._source[self._source_pos] if not self._reached_end_of_source() else
+            None
+        )
 
-    def _current_nchars(self, n) -> str:
-        return self._source[self._source_pos:self._source_pos + n] \
-            if not self._reached_end_of_source(off=n - 1) else None
+    def _current_nchars(self, n: int) -> str:
+        return (
+            self._source[self._source_pos:self._source_pos + n] if not self._reached_end_of_source(
+                off=n - 1
+            ) else
+            None
+        )
 
     @property
     def _consumed_chars(self) -> str:
         return self._source[self._source_start:self._source_pos]
 
     def _match(self, s: str) -> bool:
-        if self._reached_end_of_source(off=len(s) - 1):
-            return False
-        else:
-            return self._source[self._source_pos:self._source_pos + len(s)] == s
+        return (
+                False if self._reached_end_of_source(off=len(s) - 1) else
+                self._source[self._source_pos:self._source_pos + len(s)] == s
+        )
+
+    def _expect(self, s: str, not_met_msg: str):
+        if not self._match(s):
+            self._fmt_and_raise_syntax_error(not_met_msg)
+
+    def _fmt_and_raise_syntax_error(self, msg: str):
+        raise QuarkScannerError(
+            f'SyntaxError at {self._current_pos}: {msg}'
+        )
 
     def _consume_char(self):
         if self._reached_end_of_source():
@@ -66,48 +81,47 @@ class QuarkScanner:
     def _discard_consumed_chars(self):
         self._source_start = self._source_pos
 
-    def _reached_end_of_source(self, off=0) -> bool:
+    def _reached_end_of_source(self, off: int = 0) -> bool:
         return self._source_pos == self._source_len - off
 
     def _is_id_start(self) -> bool:
-        if self._reached_end_of_source():
-            return False
-        else:
-            return 'A' <= self._current_char <= 'Z' or 'a' <= self._current_char <= 'z' \
-                   or self._current_char == '_' or ord(self._current_char) >= 128
+        return (
+            False if self._reached_end_of_source() else
+            'A' <= self._current_char <= 'Z' or 'a' <= self._current_char <= 'z' or
+            self._current_char == '_' or ord(self._current_char) >= 128
+        )
 
     def _is_id_char(self) -> bool:
-        if self._reached_end_of_source():
-            return False
-        else:
-            return 'A' <= self._current_char <= 'Z' or 'a' <= self._current_char <= 'z' \
-                   or '0' <= self._current_char <= '9' or self._current_char == '_' \
-                   or ord(self._current_char) >= 128
+        return (
+            False if self._reached_end_of_source() else
+            'A' <= self._current_char <= 'Z' or 'a' <= self._current_char <= 'z' or
+            '0' <= self._current_char <= '9' or self._current_char == '_' or
+            ord(self._current_char) >= 128
+        )
 
     def _is_num_char(self) -> bool:
-        if self._reached_end_of_source():
-            return False
-        else:
-            return '0' <= self._current_char <= '9'
+        return (
+            False if self._reached_end_of_source() else
+            '0' <= self._current_char <= '9'
+        )
 
     def _is_str_start(self) -> bool:
-        if self._reached_end_of_source():
-            return False
-        else:
-            return self._current_char == '"'
+        return (
+            False if self._reached_end_of_source() else
+            self._current_char == '"'
+        )
 
     def _is_str_char(self) -> bool:
-        if self._reached_end_of_source():
-            return False
-        else:
-            return self._current_char != '"' and self._current_char.isascii()
-        # todo: think of restrictions
+        return (
+            False if self._reached_end_of_source() else
+            self._current_char != '"' and self._current_char.isascii()
+        )  # todo: think of restrictions
 
     def _is_skippable(self) -> bool:
-        if self._reached_end_of_source():
-            return False
-        else:
-            return self._current_char in {' ', '\t'}
+        return (
+            False if self._reached_end_of_source() else
+            self._current_char in {' ', '\t'}
+        )
 
     def next_token(self) -> Union[Token, None]:
         pos = self._current_pos
@@ -129,9 +143,9 @@ class QuarkScanner:
             must_be_real_or_zero = self._match('0')
             self._consume_char()
             while self._is_num_char():
-                if must_be_real_or_zero and not self._match('0'):
-                    raise QuarkScannerError(
-                        f'SyntaxError: leading zeros in decimal integer literals are not permitted!'
+                if must_be_real_or_zero:
+                    self._expect(
+                        '0', 'leading zeros in decimal integer literals are not permitted'
                     )
                 else:
                     self._consume_char()
@@ -158,6 +172,13 @@ class QuarkScanner:
                     type_ = TokenTypes.REAL
             else:
                 type_ = TokenTypes.PERIOD
+        elif self._is_str_start():
+            self._consume_char()
+            while self._is_str_char():
+                self._consume_char()
+            self._expect('"', 'EOL while scanning string literal')
+            self._consume_char()
+            type_ = TokenTypes.STRING
         elif self._current_nchars(3) in triple_char_tokens.keys():
             self._consume_nchars(3)
             type_ = triple_char_tokens[self._consumed_chars]
@@ -176,8 +197,8 @@ class QuarkScanner:
             else:
                 type_ = TokenTypes.ID
         else:
-            raise QuarkScannerError(
-                f'SyntaxError: invalid character {repr(self._current_char)} in identifier'
+            self._fmt_and_raise_syntax_error(
+                f'invalid character {repr(self._current_char)} in identifier'
             )
 
         ret = Token(type_, self._consumed_chars, pos)
@@ -190,11 +211,13 @@ class QuarkScanner:
             toks.append(self.next_token())
         return toks
 
+    def get_tokens(self) -> Generator[Token, None, None]:
+        while not self._reached_end_of_source():
+            yield self.next_token()
+
 
 if __name__ == '__main__':
     lexer = QuarkScanner('', ignore_skippables=False)
     while test := input('>>> '):
         lexer.reset(test, ignore_skippables=False)
-        print(
-            '\n'.join(repr(t) for t in lexer.tokens())
-        )
+        print('\n'.join(repr(t) for t in lexer.tokens()))
